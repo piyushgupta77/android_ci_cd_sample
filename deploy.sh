e58ff860ed5f1c04256887481df8cd5dc2663d65
piyush.gupta <piyushgupta77@gmail.com>
2018-06-03 01:24:18 +0530

    updating tokens

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 
-$AndroidAppId_Dev=${1:-3625d932a8864fcbb70d90e27c9e373f}
+$AndroidAppId=${1:-9a05641eacbc4be0907078e7eb805c3e}
 HockeyAppToken=e2273d142743404c99ca72626d10f89c
 
 curl \
@@ -8,4 +8,4 @@ curl \
 -F "notify=1" \
 -F "ipa=@${2:-app/build/outputs/apk/app-debug.apk}" \
 -H "X-HockeyAppToken: $HockeyAppToken" \
-https://rink.hockeyapp.net/api/2/apps/$AndroidAppId_Dev/app_versions/upload
+https://rink.hockeyapp.net/api/2/apps/$AndroidAppId/app_versions/upload
