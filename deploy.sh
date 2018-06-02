54cacd3022514c84ed66ad4050af68a766504754
piyush.gupta <piyushgupta77@gmail.com>
2018-06-03 01:13:02 +0530

    adding log message

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ $AndroidAppId_Dev=${1:-539914e75ca74b0789d2bc108635cdb5}
 HockeyAppToken=3625d932a8864fcbb70d90e27c9e373f
 
 curl \
+-w 'running curl script'
 -F "status=2" \
 -F "notify=1" \
 -F "ipa=@${2:-app/build/outputs/apk/app-debug.apk}" \
