407a914c04c9d16b7526a4c9fa107f0803e069da
piyush.gupta <piyushgupta77@gmail.com>
2018-06-03 01:03:22 +0530

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "ipa=@AppName/build/outputs/apk/app-debug.apk" \
+# -F "ipa=@AppName/build/outputs/apk/app-debug.apk" \
+# -F ""ipa=@APK FULL PATH(projectname/build/outputs/apk/app-debug.apk"
+-F "ipa=@${2:-app/build/outputs/apk/app-debug.apk}" \
 -H "X-HockeyAppToken: $HockeyAppToken" \
 https://rink.hockeyapp.net/api/2/apps/$AndroidAppId_Dev/app_versions/upload
