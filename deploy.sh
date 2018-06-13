4ac94335c1605ba56507883be8e8d94fd529851e
piyush.gupta <piyushgupta77@gmail.com>
2018-06-13 23:13:45 +0530

    uploading efica file

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 
-$AndroidAppId=${1:-9a05641eacbc4be0907078e7eb805c3e}
-HockeyAppToken=e2273d142743404c99ca72626d10f89c
+$AndroidAppId=${1:-3625d932a8864fcbb70d90e27c9e373f}
+HockeyAppToken=fa73392bec644ec7aa2340d7d95a50fc
 
 curl \
 -w 'running curl script'
 -F "status=2" \
 -F "notify=1" \
--F "ipa=@${2:-app/build/outputs/apk/app-debug.apk}" \
+-F "ipa=@${2:-app/build/outputs/apk/efica.apk}" \
 -H "X-HockeyAppToken: $HockeyAppToken" \
 https://rink.hockeyapp.net/api/2/apps/$AndroidAppId/app_versions/upload
