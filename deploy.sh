34602c0f5a98d701c2b8c57b71b3355162bf6e3d
piyush.gupta <piyushgupta77@gmail.com>
2018-06-14 23:41:38 +0530

    deploy hockey app 1

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ curl \
 -F "status=2" \
 -F "notify=1" \
 -F "ipa=@${2:-app/build/outputs/apk/efica.apk}" \
--H "X-HockeyAppToken: $HockeyAppToken" \
-https://rink.hockeyapp.net/api/2/apps/$AndroidAppId/app_versions/upload
+-H "X-HockeyAppToken: 8d419555305146d99f4ff38f3e122572" \
+https://rink.hockeyapp.net/api/2/apps/8bcc6c28760b411aae652638cae2e274/app_versions/upload
