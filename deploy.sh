b97536e3e399d1e192030e5b13fe1461a7077a34
piyush.gupta <piyushgupta77@gmail.com>
2018-06-15 00:49:40 +0530

    changing apk file name

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ curl \
 -w 'running curl script'
 -F "status=2" \
 -F "notify=1" \
--F "ipa=@${2:-app/build/outputs/apk/efica.apk}" \
+-F "ipa=@${2:-app/build/outputs/apk/app-debug.apk}" \
 -H "X-HockeyAppToken: 8d419555305146d99f4ff38f3e122572" \
 https://rink.hockeyapp.net/api/2/apps/8bcc6c28760b411aae652638cae2e274/app_versions/upload
