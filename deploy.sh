ea16b3ab1351d9b1f437459f9147400ea6818204
piyush.gupta <piyushgupta77@gmail.com>
2018-06-15 16:50:16 +0530

    trying renaming apk file

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 curl \
 -F "status=2" \
 -F "notify=1" \
--F "ipa=@${2:-app/build/outputs/apk/debug/app-debug.apk}" \
+-F "ipa=@${2:-app/build/outputs/apk/debug/efica.apk}" \
 -H "X-HockeyAppToken: 8d419555305146d99f4ff38f3e122572" \
 https://rink.hockeyapp.net/api/2/apps/8bcc6c28760b411aae652638cae2e274/app_versions/upload
