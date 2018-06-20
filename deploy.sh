f1d2e8a54bdb8bd5823cfd527800b9b1b91e4c61
piyush.gupta <piyushgupta77@gmail.com>
2018-06-20 15:19:22 +0530

    uploading new ssh files

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 curl \
 -F "status=2" \
 -F "notify=1" \
--F "ipa=@${2:-/home/circleci/project/outputs/outputs/apk/debug/efica.apk}" \
+-F "ipa=@${2:-/home/circleci/project/outputs/outputs/apk/debug/app-debug.apk}" \
 -H "X-HockeyAppToken: 8d419555305146d99f4ff38f3e122572" \
 https://rink.hockeyapp.net/api/2/apps/8bcc6c28760b411aae652638cae2e274/app_versions/upload
