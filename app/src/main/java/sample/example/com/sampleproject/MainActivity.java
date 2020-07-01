feff76084d8b0eaa58167c1aef4ef0551605d322
piyush.gupta <piyush.gupta01@nagarro.com>
2020-07-01 22:06:51 +0530

    ci test 2

--- app/src/main/java/sample/example/com/sampleproject/MainActivity.java
+++ app/src/main/java/sample/example/com/sampleproject/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // new comment 1
+        // new comment 2
     }
 }
